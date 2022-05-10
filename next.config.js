d196c03da594c932e665fdda6522efb31b039c15
datquocngo1409 <shaco14091999@gmail.com>
2022-05-10 18:09:07 +0700

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/lfr3/eg-services/:path*',
+        destination: 'https://int.logifleet360.ch/lfr3/eg-services/:path*' // Proxy to Backend
+      }
+    ]
+  }
 }
-
-module.exports = nextConfig
